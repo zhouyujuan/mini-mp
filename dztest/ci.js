4e6c0cceebc4c2f641e1f1422f04eb0a94f8f987
zhouyujuan <zhouyujuan@baijia.com>
2021-06-30 19:14:20 +0800

    修改构建后的图片的名字

--- dztest/ci.js
+++ dztest/ci.js
@@ -46,7 +46,7 @@ const project = new ci.Project({ ...reqParams });
     desc: '预览', // 此备注将显示在“小程序助手”开发版列表中
     setting: uploadParams,
     qrcodeFormat: 'image',
-    qrcodeOutputDest: `./qrcode-${buildId}.jpg`,
+    qrcodeOutputDest: `./qrcode.jpg`,
     onProgressUpdate: console.log,
     // pagePath: 'pages/index/index', // 预览页面
     // searchQuery: 'a=1&b=2',  // 预览参数 [注意!]这里的`&`字符在命令行中应写成转义字符`\&`
